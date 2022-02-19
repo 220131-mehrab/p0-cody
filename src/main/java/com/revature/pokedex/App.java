9fb0882afa2d57e39a7aab44587ad2bd0cc37551
CodyBenko <98978890+codybenko@users.noreply.github.com>
2022-02-18 20:25:44 -0500

    Create gradle.yml

--- src/main/java/com/revature/pokedex/App.java
+++ src/main/java/com/revature/pokedex/App.java
@@ -6,17 +6,19 @@ import org.apache.catalina.startup.Tomcat;
 
 public class App {
     public static void main(String[] args){
+        String webAppName = "pokedex";
         DexRepository dexRepository = new DexRepository("pokedex.csv");
         DexService dexService = new DexService(dexRepository);
         SearchFormService sfService = new SearchFormService();
 
         Tomcat server = new Tomcat();
         server.setBaseDir(System.getProperty("java.io.tmpdir"));
-
+        server.setPort(0);
         server.getConnector();
-        server.addContext("", null);
-        server.addServlet("", "dexServlet", dexService).addMapping("/pokemon");
-        server.addServlet("", "SearchFormServlet", sfService).addMapping("/search");
+        server.addContext(webAppName, null);
+        server.addServlet(webAppName, "defaultServlet", new DefaultServlet()).addMapping("/*");
+        server.addServlet(webAppName, "dexServlet", dexService).addMapping("/pokemon");
+        server.addServlet(webAppName, "SearchFormServlet", sfService).addMapping("/search");
         try {
             server.start();
         } catch (LifecycleException e) {
---
+++ src/main/java/com/revature/pokedex/Pokemon.java
@@ -0,0 +1,17 @@
+package com.revature.pokedex;
+
+public class Pokemon {
+    private String name;
+    public Pokemon(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
